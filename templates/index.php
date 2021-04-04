382967408c43202cfcde14a16e56e4b96cb596b0
Davin <davin.hayden@outlook.com>
2021-04-05 00:39:24 +0100

    updated index to refer to questions, and updaed QuestionModelFactory to use QuestionModel

--- templates/index.php
+++ templates/index.php
@@ -3,15 +3,15 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>To Do List</title>
+    <title>Core Question Form</title>
     <link href="style.css" type="text/css" rel="stylesheet">
     <link href='//fonts.googleapis.com/css?family=Lato:400' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-    <h1>To Do List</h1>
+    <h1>Core Question Form</h1>
 
-    <h2>Add Task</h2>
+    <h2>Questions - TBC</h2>
     <form method="post" action="/saveTask">
         <label for="item">Task:</label>
         <input type="text" name="item" id="item">
@@ -19,20 +19,31 @@
     </form>
     <br>
 
-    <h2>Tasks To Do</h2>
+    <h2>Question Table</h2>
+    <!-- need a table here for Questions -->
     <?php
+    echo '<ol>';
+    foreach($coreQuestions as $coreQ) {
+        echo '<li>' . $coreQ["question"] . ' PointsType ' . $coreQ["points_type"] .' </li>';
+        }
+    echo '</ol>';
+    ?>
+
+    <h2>Tasks To Do</h2>
+    <!-- <?php
     echo '<ol>';
     foreach($tasks as $task) {
     //        echo '<li>' . $task["item"] . '</li>';
     //        echo '<a href="/markAsComplete/' . $task["id"] . '">Completed</a>';
+
     //show task in a list, with a link to Complete each task
         echo '<li>' . $task["item"] . ' &nbsp <a href="/markAsComplete/' . $task["id"] . '">Completed</a> </li>';
     }
     echo '</ol>';
-    ?>
+    ?> -->
 
     <br><br>
-    <h3><a href="/completedTasks">View Completed Tasks</a></h3>
+    <h3><a href="/completedTasks">View Historical QA - Later</a></h3>
 
 </body>
 </html>
