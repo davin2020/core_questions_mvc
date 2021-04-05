81618075e974a0bc07da82084d08f4590368f002
Davin <davin.hayden@outlook.com>
2021-04-05 15:10:47 +0100

    created UserModel & Factory, updated SaveUser & Homepage Controller & Factory, so main index.php can  now add new uses to DB and displays existing DB users ok

--- templates/index.php
+++ templates/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <html>
-<!-- Davin updated this file for CoreQuestions -->
+<!-- Davin updated this file for CoreQuestions - BUT style.css is not being found! -->
 <head>
     <meta charset="utf-8"/>
     <title>Core Question Form</title>
+    <!-- <link href="/templates/style2.css" type="text/css" rel="stylesheet"> -->
     <link href="style.css" type="text/css" rel="stylesheet">
     <link href='//fonts.googleapis.com/css?family=Lato:400' rel='stylesheet' type='text/css'>
 </head>
@@ -12,10 +13,24 @@
 <body>
     <h1>Core Question Form</h1>
 
-    <h2>Add A New User - TBC</h2>
-    <form method="post" action="/saveTask">
-        <label for="item">Task:</label>
-        <input type="text" name="item" id="item">
+    <h2>Show Existing Users</h2>
+    <!-- hwo to call this ??? the Controller for this page ie HomePageController is where these variables are coming from eg $userList -->
+    <?php
+    echo '<ul>';
+    foreach($usersList as $user) {
+        echo '<li>' . $user["name"] . ' Date Joined: ' . $user["date_joined"] .' </li>';
+        }
+    echo '</ul>';
+    ?>
+
+    <h2>Add A New User</h2>
+    <!-- where is item being refered to, when getting data from Form ? -->
+    <form method="post" action="/saveUser">
+        <label for="item">Name of New User:</label>
+        <input type="text" name="itemName" id="itemName">
+        <br>
+        <label for="item">Date Joined for New User:</label>
+        <input type="Date" name="itemDate" id="itemDate">
         <button name="btnAddItem" type="submit">Add</button>
     </form>
     <br>
