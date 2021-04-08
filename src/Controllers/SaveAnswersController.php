12cff98902fa2d7e94253d49a9f51d625d1e13a8
Davin <davin.hayden@outlook.com>
2021-04-08 12:05:18 +0100

    got question form working with selected existing user and todays date, which gets saved to relevant tables when form is submitted

--- src/Controllers/SaveAnswersController.php
+++ src/Controllers/SaveAnswersController.php
@@ -14,6 +14,12 @@ class SaveAnswersController
 
     public function __invoke($request, $response, $args)
     {
+        //NEW TODO need to get userid and date form completed, then add those args to the saveAnswers function
+// existingUser
+// dateCompleted
+        $userID = $request->getParsedBody()['existingUserID'];
+        $dateFormCompleted = $request->getParsedBody()['dateCompleted'];
+
         //this works - i only want the selected radio button from teh whole group!
         $dataArrayAnswers = $request->getParsedBody()['radioAnswerPoints'];
         // var_dump($dataArrayAnswers);
@@ -29,15 +35,15 @@ class SaveAnswersController
         // exit;
 
         //need to collect userid and date from Form - how to get Date object and not String for the date?
-        $userID = 1;
-        $scoreDate = "2021-04-05";
+        // $userID = 1;
+        // $scoreDate = "2021-04-05";
         // $dataAnswerNot = $request->getParsedBody()['answerNot'].value;
 
 
         //needs to save ARRAY of values to db ie $dataArrayAnswers - and save to tables user_core_answers and user_core_score 
         // public function saveAnswers(int $userID, string $scoreDate, array $dataArrayAnswers, int $totalScore)
 
-        $result = $this->answerModel->saveAnswers($userID, $scoreDate, $dataArrayAnswers, $totalScore);
+        $result = $this->answerModel->saveAnswers($userID, $dateFormCompleted, $dataArrayAnswers, $totalScore);
 
         //redirects back to homepage, no need to render anything! ./ means current page, / means root/main page
         return $response->withHeader('Location', '/')->withStatus(302);
