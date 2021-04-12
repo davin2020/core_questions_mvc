ed30790aa0aec82f9b26a35b3698186e2fb50e67
Davin <davin.hayden@outlook.com>
2021-04-12 13:57:07 +0100

    tidied up code wrt indenting part 1 - changed to tabs for indenting and organised indent levels better

--- src/Factories/HomepageControllerFactory.php
+++ src/Factories/HomepageControllerFactory.php
@@ -1,22 +1,21 @@
 <?php
 
-
 namespace App\Factories;
-// Davin updated this file for CoreQuestions wrt Qs and UserModel
+// Davin updated this file for CoreQuestions wrt QuestionModel and UserModel
 
 use App\Controllers\HomepageController;
 
 class HomepageControllerFactory
 {
-    public function __invoke($container)
-    {
-        //ok to break DI here, inside factory, in order to create new controller
-        //get dependancceis from DIC/container, then call constructor to create new controller & return it
-        $userModel = $container->get('UserModel');
-        $questionModel = $container->get('QuestionModel');
-        $renderer = $container->get('renderer');
-        $homepageController = new HomepageController($userModel, $questionModel, $renderer);
-        return $homepageController;
-    }
+	public function __invoke($container)
+	{
+		//ok to break DI here, inside factory, in order to create new controller
+		//get dependancceis from DIC/container, then call constructor to create new controller & return it
+		$userModel = $container->get('UserModel');
+		$questionModel = $container->get('QuestionModel');
+		$renderer = $container->get('renderer');
+		$homepageController = new HomepageController($userModel, $questionModel, $renderer);
+		return $homepageController;
+	}
 
 }
--- src/Factories/SaveAnswersControllerFactory.php
+++ src/Factories/SaveAnswersControllerFactory.php
@@ -1,20 +1,20 @@
 <?php
 
-
 namespace App\Factories;
-// updated for CoreQuestions wrt SaveAnswers
+// updated for CoreQuestions wrt AnswerModel which saves users answers to questions 
 
 use App\Controllers\SaveAnswersController;
 
 class SaveAnswersControllerFactory
 {
-    public function __invoke($container)
-    {
-        //always needs to instantiate a new controller inside factory's invoke method
-        //no renderer is required here, as we only need to redirect to homepage after saving a new user, ie no need to display anything specific to saving a user - for Questions would be better to show a success message after saving!
-        $answerModel = $container->get('AnswerModel');
-        $saveAnswersController = new SaveAnswersController($answerModel);
-        return $saveAnswersController;
-    }
+	public function __invoke($container)
+	{
+		//always needs to instantiate a new controller inside factory's invoke method
+		//no renderer is required here, as we only need to redirect to homepage after saving the users answers, ie no need to display anything specific to saving them - Actually it would be better to show a success message after saving the Answers, but how?
+		$answerModel = $container->get('AnswerModel');
+		// $renderer = $container->get('renderer');
+		$saveAnswersController = new SaveAnswersController($answerModel);
+		return $saveAnswersController;
+	}
 
 }
