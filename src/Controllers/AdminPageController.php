b8326e55efbc81351e248a3f23da79dc794d9d26
Davin <davin.hayden@outlook.com>
2021-08-23 23:38:50 +0100

    adjusted sessions & error msgs if user not logged in or not an admin user

--- src/Controllers/AdminPageController.php
+++ src/Controllers/AdminPageController.php
@@ -30,14 +30,21 @@ class AdminPageController
 		session_start(); 
 		echo session_id();
 
-		if ( !$_SESSION['coreIsLoggedIn'] || $_SESSION['userId'] != 10 ) {
-			$_SESSION['errorMessage'] = 'Trying to access ADMIN page and your not logged in or your not Davin, so redirected from Admin Controller to Index';
+		// if user isnt logged in at all, redirect to homepage so they can login
+		if ( !$_SESSION['coreIsLoggedIn'] ) {
+			$_SESSION['errorMessage'] = 'Trying to access a Restricted page and your not an Admin User,  so redirected back to Homepage';
 			//when redirection happens and there is no session, then no $errorMessage will be sent - could try redirecting to a third page, just to prove that works
 			header("Location: /"); //must be a route not a view
 	    	exit();
 		}
+		// If they ARE logged in BUT are not an ADMIN user, send them back to dashboard?
+		if ( $_SESSION['coreIsLoggedIn'] && $_SESSION['userId'] != 10 ) {
+			$_SESSION['errorMessage'] = 'Trying to access a Restricted page and your not an Admin User,  so redirected back to Dashboard';
+			header("Location: /dashboard");
+			exit();
+		}
 		// else if ( !$_SESSION['coreIsLoggedIn'] && $_SESSION['userId'] ) {
-		//only show contents of admin page if 
+		//only show contents of admin page if its AdminDavin
 		else if ( $_SESSION['coreIsLoggedIn'] && $_SESSION['userId'] == 10) {
 			echo "<br>DAVIN is logged in as ADMIN";
 		}
