7a8ab3e001494bb927e701211042ccce8a98fab0
Davin <davin.hayden@outlook.com>
2021-08-23 23:26:47 +0100

    added sessions to AdminPageController and redirect  to index if the admin user isnt logged in; added Logout button to View admin.php

--- src/Controllers/AdminPageController.php
+++ src/Controllers/AdminPageController.php
@@ -27,6 +27,22 @@ class AdminPageController
 	//invoke only ever does 1 thing, each page has own controler thus own invoke method, where the stuff for the actual page gets done eg display users & questions or save answers etc
 	public function __invoke($request, $response, $args)
 	{
+		session_start(); 
+		echo session_id();
+
+		if ( !$_SESSION['coreIsLoggedIn'] || $_SESSION['userId'] != 10 ) {
+			$_SESSION['errorMessage'] = 'Trying to access ADMIN page and your not logged in or your not Davin, so redirected from Admin Controller to Index';
+			//when redirection happens and there is no session, then no $errorMessage will be sent - could try redirecting to a third page, just to prove that works
+			header("Location: /"); //must be a route not a view
+	    	exit();
+		}
+		// else if ( !$_SESSION['coreIsLoggedIn'] && $_SESSION['userId'] ) {
+		//only show contents of admin page if 
+		else if ( $_SESSION['coreIsLoggedIn'] && $_SESSION['userId'] == 10) {
+			echo "<br>DAVIN is logged in as ADMIN";
+		}
+		//need logout button
+
 		//need to return response with args[] via render method - can have multiple named keys if u needed to add/display more stuff later
 		$assocArrayArgs = [];
 
