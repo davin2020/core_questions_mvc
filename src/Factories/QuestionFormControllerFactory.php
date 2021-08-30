3ad25cb6017ea6751e54952459b792a781b7911c
Davin Stirling <65677163+davin2020@users.noreply.github.com>
2021-08-31 00:41:36 +0100

    Revert "displayed history data in table and updated styling"

--- src/Factories/QuestionFormControllerFactory.php
+++ src/Factories/QuestionFormControllerFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Factories;
+// Davin updated this file for CoreQuestions wrt QuestionModel and UserModel
 
 use App\Controllers\QuestionFormController;
 
@@ -8,6 +9,8 @@ class QuestionFormControllerFactory
 {
 	public function __invoke($container)
 	{
+		//ok to break DI here, inside factory, in order to create new controller
+		//get dependancceis from DIC/container, then call constructor to create new controller & return it
 		$userModel = $container->get('UserModel');
 		$questionModel = $container->get('QuestionModel');
 		$renderer = $container->get('renderer');
