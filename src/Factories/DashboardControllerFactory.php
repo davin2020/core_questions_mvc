b9162de2544051725101a1c79ad30c4f2994735f
Davin <davin.hayden@outlook.com>
2021-08-30 23:53:43 +0100

    removed extra comments from Factories to tidy up code

--- src/Factories/DashboardControllerFactory.php
+++ src/Factories/DashboardControllerFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Factories;
-// Davin updated this file for CoreQuestions wrt QuestionModel and UserModel
 
 use App\Controllers\DashboardController;
 
@@ -9,10 +8,7 @@ class DashboardControllerFactory
 {
 	public function __invoke($container)
 	{
-		//ok to break DI here, inside factory, in order to create new controller
-		//get dependancceis from DIC/container, then call constructor to create new controller & return it
 		$userModel = $container->get('UserModel');
-		// $questionModel = $container->get('QuestionModel');
 		$renderer = $container->get('renderer');
 		$dashboardController = new DashboardController($userModel, $renderer);
 		return $dashboardController;
