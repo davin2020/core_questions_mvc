c639b8703416700ebdc4d93348a348e5a5ed23f3
Davin <davin.hayden@outlook.com>
2021-09-06 19:52:34 +0100

    removed extra functions no longer being used & reduced comments

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -51,44 +51,9 @@ class UserModel
 		// Set the fetch mode right after you call prepare(). It appears you _must_ use execute() - fetch() won't work
 		//this is better way of doing fetch, w assoc array
 		$result = $query->fetch(\PDO::FETCH_ASSOC);
-		// $result = $query->execute();
-		// $result = $query->fetch();
 		return $result;
 	}
 
-
-	//chck this out 16may2021
-	/*
-
-	https://www.php.net/manual/en/pdostatement.setfetchmode.php
-	To fetch the rows into an existing instance of a class, use PDO::FETCH_INTO and pass the object as the second parameter.
-
-	The class _must_ have the column names declared as public members, or the script will die. But overloading with __set() and __get() lets it handle any column your query throws at it. 
-
-	Set the fetch mode right after you call prepare(). It appears you _must_ use execute() - fetch() won't work. A small example, adapted from ext/pdo/tests/pdo_025.phpt:
-
-	class Test
-	{
-	    protected $cols;
-	   
-	    function __set($name, $value) {
-	        $this->cols[$name] = $value;
-	    }
-	   
-	    function __get($name) {
-	        return $this->cols[$name];
-	    }
-	}
-
-	$obj = new Test();
-	$db = PDOTest::factory();
-	$stmt = $db->prepare("select * from test");
-	$stmt->setFetchMode(PDO::FETCH_INTO, $obj);
-	$stmt->execute();
-
-	*/
-
-	// 13may2021 also need func getUserFromEmail(string $emailAddress)
 	public function getUserByEmail(string $userEmail)
 	{
 		$query = $this->db->prepare('SELECT * FROM `users` 
@@ -99,20 +64,14 @@ class UserModel
 		// ok this works to return data lik this - 
 		// array(6) { ["user_id"]=> string(2) "30" ["fullname"]=> string(5) "test6" ["nickname"]=> string(5) "test6" ["email"]=> string(17) "test6@example.com" ["password"]=> string(60) "$2y$10$drABEk1kJR5WDgjPWDVCt.Xn1jmwbBeUIgEpCOONxQrfo33CXZh8q" ["date_joined"]=> string(10) "2021-05-15" }
 		$result = $query->fetch(\PDO::FETCH_ASSOC);
-		// $result = $sth->fetch(PDO::FETCH_ASSOC);
-		//FETCH_ASSOC
-
-		//before returning should i check the pwd matches here??
-		// or shoudl i have multiple methods eg getUserByEmail, verifyPassword and call tehm from another method eg loginUser(), but then what/how to return values so the caller can deal with user existing or not, and pwd being correct or not?
 		return $result;
 	}
 
 	//should i salt & hash pwd in UserModel or SaveUserController? - prob best to hash it as soon as u receive it  - its really part of Model, not Controller - but now thsi function does 2 things, hashes pwd & save/registers user!
 	// what about PSR formatting for long type hinted method signatures over 80 chars long?
+	// TODO form need validation to stop future dates from being entered!
 	public function registerUser(string $fullname, string $nickname, string $email, string $hashedPassword, string $dateJoinedToday) 
 	{
-		// pwd has already been hashed
-		//$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
 		$lastID = 0; // non valid id - issue with below stmt was typo!
 		$query = $this->db->prepare('INSERT INTO `users` 
@@ -128,7 +87,7 @@ class UserModel
 			'pl_email' => $email, 
 			'pl_password' => $hashedPassword, 
 			'pl_date_joined' => $dateJoinedToday]);
-		//fyi date being inserted is 14may despite today being 00:30 on 15may, as its using UTC not current local timezone
+		// ISSUE date being inserted is 14may despite today being 00:30 on 15may, as its using UTC not current local timezone! how/where to set/use timezone?
 
 		if ($result) {
 			$lastID = $this->db->lastInsertId();
@@ -136,35 +95,6 @@ class UserModel
 		return $lastID;
 	}
 
-	/*
-	public function loginUser(string $userEmail, string $userPassword)
-	{
-		//either return teh user or return false?
-		$userResultsArray = [];
-
-		$existingUser = $this->getUserByEmail($userEmail);
-
-		//this now returns a bool of true, so how to get the user that was returned from the db?
-		// $tempUser = $this->userModel->getUserFromID(30);
-		
-
-		//check pwds here on in UserModel.getUserbyEmail or LoginUser? in UserModel
-		$existingHashedPassword = $existingUser['password'];
-		$user_id = $existingUser['user_id'];
-		// $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-		//compare pwd from user form against its hash from db
-		$pwdMatches = password_verify ($userPassword, $existingHashedPassword);
-
-		//now return something based on the match?
-		$userResultsArray['pwdMatches'] = $pwdMatches;
-		$userResultsArray['existingUser'] = $existingUser;
-		
-
-		//why dont i decide where to redirect the user to next in here, then just return the next page to go to?? thsi all seems overly complex! when surely its easier for the controller to decide
-		return $userResultsArray;
-	}
-	*/
 
 	// separate function to hash passwords so can be called from any controller, also function has single responsibilty instead of being part of saveUser()
 	public function hashPassword(string $newPassword):string {
@@ -176,27 +106,8 @@ class UserModel
 		return password_verify ($userSuppliedPassword, $existingDbPassword);
 	}
 
-/*
-URLS
-$userResultsArrayTemp = [];
-		if ($doesPwdMatch) {
-			return $response->withHeader('Location', '/dashboard/' . $user_id)->withStatus(200);
-		}
-		if (!$doesUserExist) {
-			$userResultsArrayTemp['messageForUser'] = "user does not exist, please check spelling and try again";
-			return $this->renderer->render($response, 'index.php', $userResultsArrayTemp);
-		}
-		else {
-			$userResultsArrayTemp['messageForUser'] = "wrong password, please try again";
-		return $this->renderer->render($response, 'index.php', $userResultsArrayTemp);
-		}
 
-*/
-
-
-	// TODO form need validation to stop future dates from being entered
-	// should date be passsed in as a string, or a Date object?
-		// this is orignal function now on Admin page, hence only name & date being saved
+	// Original function, only called by Admin Page, so only name & date being saved
 	public function saveUser(string $user, string $date_joined)
 	{
 		$query = $this->db->prepare('INSERT INTO `users` 
